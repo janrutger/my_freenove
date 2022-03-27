87b742cbda1b1e6be32f3573bfe723bb4d067036
“janrutger” <janrutger@jaruku.nl>
2022-03-27 13:22:20 +0200

    new brain v1.0

--- Brain.py
+++ Brain.py
@@ -4,17 +4,17 @@ from numpy import average as avg
 
 class Brain:
     def __init__(self):
-        self.maxDistance = 60
+        self.maxDistance = 80
         self.minDistance = 30
 
     def drive(self, distances):
         print("gemeten afstand:", distances)
-        if min(distances[0]) < self.minDistance: #all vallues unsinged
+        if min(distances[0]) < self.minDistance: #all vallues unsigned
             return((0,0,0,0), True)
         else:
             leftDistances  = []
             rightDistances = []
-            for d in distances[1]:  #only singed values
+            for d in distances[1]:  #only signed values (delta angle != 0)
                 if d < 0:
                     if abs(d) < self.maxDistance:
                         leftDistances.append(d)
@@ -32,10 +32,10 @@ class Brain:
                 leftPower  =  round((avg(leftDistances) / self.maxDistance)*100)
                 leftPower  = abs(leftPower) * 1.0
                 rightPower = round((avg(rightDistances) / self.maxDistance)*100)
-                rightPower = rightPower * 0.8
+                rightPower = rightPower * 0.5
             elif avg(leftDistances) + avg(rightDistances) < 0:
                 leftPower  =  round((avg(leftDistances) / self.maxDistance)*100)
-                leftPower  = abs(leftPower) * 0.8
+                leftPower  = abs(leftPower) * 0.5
                 rightPower = round((avg(rightDistances) / self.maxDistance)*100)
                 rightPower = rightPower * 1.0
             else:
