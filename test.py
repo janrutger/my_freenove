07d34900fcba52a60aadc4e90e635ea7b4a84ed6
“janrutger” <janrutger@jaruku.nl>
2022-04-07 22:28:38 +0200

    brain (v1.51)

---
+++ test.py
@@ -0,0 +1,18 @@
+naam = "Pietje"
+print("hallo ", naam)
+
+A = 10
+B = 15
+print(A+B)
+print(A*B)
+
+if A < B:
+    print("kleiner")
+else:
+    print(naam)
+
+
+namen = ["ria", "Piet", "JanRutger", "Loise"]
+
+for X in namen:
+    print(X)
